98d03e5ce5b1b729344a868242c8f62b195f499e
MxCabrera <53961785+MxCabrera@users.noreply.github.com>
2020-04-16 21:22:14 -0400

    miguels first commit

---
+++ script/script.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+
+let score = 0;
+let mainQuestion = 0;
+console.log('it works motha fuckaaa!');
+
+triviaApp.apiUrl = `https://opentdb.com/api.php`;
+
+// get data from the trivia API - ajax request
+
+
+
