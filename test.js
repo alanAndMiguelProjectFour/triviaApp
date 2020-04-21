f920c77f6dd64c65319dadaf7ca4c0a4a1103c07
Alan <alan.edward.martin@gmail.com>
2020-04-21 03:47:29 -0400

    fixed issues with api call not finishing before function calls causeing undefined, added askQuestion function which pushes a question and a random array of answers to the dom while also setting a data- to validate if correct or not. also added a questionCounter to currentQuestion to iterate through questions/answers arrays, then added a pickAnswer function that determines if users choice is correct or not and adds ++ to he question counter to then load a new question! - ALAN GO SLEEP NOW LONG DAY

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
 
 // name spacing object
-const triviaApp = {}
+const triviaApp = {};
 // global variables for API
-triviaApp.data = []
-triviaApp.dataQuestions = [];
+triviaApp.data = [];
+triviaApp.questions = [];
 triviaApp.incorrect = [];
 triviaApp.correct = [];
 // this vairable determines what question the player is currently on
-triviaApp.currentQuestion = 0;
+triviaApp.questionCounter = 0;
 // this keeps track of the amount of right questions the player has selected
 triviaApp.playerScore = 0;
 // users answer choice
@@ -16,78 +16,91 @@ triviaApp.userChoice;
 // API URL
 triviaApp.apiUrl = `https://opentdb.com/api.php`;
 
-
 // function calls API
-triviaApp.getQuestion = () => {
+triviaApp.getQuestions = () => {
   $.ajax({
     url: triviaApp.apiUrl,
-    method:'GET',
+    method: 'GET',
     dataType: 'json',
     data: {
       amount: 10,
       category: 14,
       type: 'multiple',
-    }
+    },
   }).then(array => {
-    // pushes all data need into triviaApp.data array
-    triviaApp.data.push(array.results)
-    // loops through all questions and stores them in a questions array
-    for(let i= 0; i < array.results.length; i++) {
-      triviaApp.dataQuestions.push(array.results[i].question)
-  
-      // interates through all questions and makes an takes an array of correct answers
-      triviaApp.correct.push(array.results[i].correct_answer)
-      
-      // interates through all questions and makes an takes an array of incorrect answers to triviaApp.incorrect array
-      triviaApp.incorrect.push(array.results[i].incorrect_answers)
+    triviaApp.createQuestions = () => {
+      // pushes all data need into triviaApp.data array
+      triviaApp.data.push(array.results);
+      // loops through all questions and stores them in a questions array
+      for (let i = 0; i < array.results.length; i++) {
+        triviaApp.questions.push(array.results[i].question);
+        // interates through all questions and makes an array of correct answers
+        triviaApp.correct.push(array.results[i].correct_answer);
+
+        // interates through all questions and makes an takes an array of incorrect answers to triviaApp.incorrect array
+        triviaApp.incorrect.push(array.results[i].incorrect_answers);
+      }
     };
-    triviaApp.createQuestions(array.results)
-    // console.log(triviaApp.incorrect)
-    
-  }).fail((error) => {
-    console.log(error);
+    // creates questions array, correct answers, and incorrect answers
+    triviaApp.createQuestions();
+    // this is called here to give time for the get request to be completed or arrays will be empty
+    triviaApp.askQuestion();
   });
 };
 
-triviaApp.createQuestions = (hello) => {
-  for (let i = 0; i < hello.length; i++){
-  $("#question").html(`<h2>${hello[0].question}</h2>`);
-  console.log(hello[0].question)
+// appends question to question container
+triviaApp.askQuestion = () => {
+  // use question/answer current question value to itterate through array of questions
+
+  // save the currentQuestion in a variable
+  const currentQuestion = triviaApp.questions[triviaApp.questionCounter];
+  // save answer in variable
+  const currentAnswer = triviaApp.correct[triviaApp.questionCounter];
+  // save incorrect questions in variable
+  const incorrectAnswersArray = triviaApp.incorrect[triviaApp.questionCounter];
+
+  console.log('QUESTION', currentQuestion, 'ANSWER',currentAnswer, 'WRONG ANSWERS',incorrectAnswersArray);
+
+  //append current question to dom!
+  $('.currentQuestion').html(currentQuestion)
+
+  // create a new array of answers with both incorrect and correct answers
+  possibleAnswerChoices = incorrectAnswersArray.concat([currentAnswer])
+  possibleAnswerChoices.sort()
+
+  console.log('All possible answers!',possibleAnswerChoices);
+
+  // loop through all answers and put them into individual p tags
+  for (let i = 1; i <= possibleAnswerChoices.length; i++) {
+    // set dataset value to each choice to verify if correct
+    $(`#answerOpt-${i}`).attr('data-answer', possibleAnswerChoices[i - 1])
+    //append choices to dom
+    $(`#answerOpt-${i}`).html(possibleAnswerChoices[i -1])
   }
-  // console.log(triviaApp.data)
-  hello.forEach((e) => {
-    // $("#questions").html(`<h2>${questions}</h2>`);
-    // console.log(e)
-  });
-  // let questionObject = triviaApp.dataQuestions;
-  // console.log(questionObject)
-  // for(let i = 0; i < triviaApp.dataQuestions.length; i++) {
-  //   triviaApp.questions = triviaApp.dataQuestions;
-  //   console.log(triviaApp.questions[0])
-  // }
+
+};
+
+// submits answer on user selection
+triviaApp.pickAnswer = () => {
+  // get a value from players
+  $('.possibleAnswers').on('click keypress', function(e) {
+    // if else statement will determine if answer is right by comparing data set values if right then playerScore++ if wrong then playerscore is not given anything, else questionCounter++ then call next question in both circumstances!
+    console.log(this.dataset.answer === triviaApp.correct[triviaApp.questionCounter],);
+    // $('.possibleAnswer').toggleClass('correct')
+    setTimeout(()=> {
+      triviaApp.questionCounter++;
+      console.log(triviaApp.questionCounter);
+    },3000)
+  })
+
 }
 
-
-triviaApp.startGame = () => {
-  //calls API and populates empty arrays
-  triviaApp.getQuestion();
-  // populate dom with questions and answers
-  triviaApp.createQuestions();
-}
-
-
-$('#answerChoices').on('click', function(){
-  triviaApp.currentQuestions++
-  triviaApp.createQuestions(triviaApp.data);
-})
-
-// this hard reloads page on click
-$('#restart').on('click', function () {
-  location.reload();
-});
+triviaApp.pickAnswer()
 
 // doc ready function
-$(function(){
-  triviaApp.startGame();
-
+$(function() {
+  triviaApp.getQuestions();
 });
+
+
+// miguel I was able to fix the functionality and make the array load properly so we could use the data! I was also able to append the info to the dom and make the correct answer and incorrect answer a single array then randomize them with.sort then I assigned them data values equaul to the current questiin value! its looking good. the question counter will then iterate trhough all qoustions!! i also made the design a bit cleaner becayse i had to change the names on the html to fit the js info from the array inside. Take a look and let me know what you think - ALAN
